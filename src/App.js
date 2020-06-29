2e986f79a919447ed7c865de5645a02b77aeac8c
RickardThomas <tom-rickard@hotmail.co.uk>
2020-06-29 09:40:00 +0100

    Updated commits

--- src/App.js
+++ src/App.js
@@ -1,110 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useState } from 'react'
 import TradingViewWidget from 'react-tradingview-widget';
+
+const links = [ {text: 'CoinFarm'    , name:"https://coinfarm.online/index.asp?tcs=1h"},
+                {text: 'CMC'         , name:"https://coinmarketcap.com/" }, 
+                {text: 'Bitfinex'    , name: "https://www.bitfinex.com/trading"}, 
+                {text: 'Bids vs Sums', name:"http://data.bitcoinity.org/markets/bidask_sum/7d/USD/coinbase?bp=5&bu=c&t=m"}]
+
+const indicator = { one: [ "MAVolumeWeighted@tv-basicstudies", "BollingerBandsR@tv-basicstudies"],
+                    two: [ "RSI@tv-basicstudies",  "CMF@tv-basicstudies"]}
+
+const sym = ["COINBASE:ETHEUR", "COINBASE:BTCEUR", "COINBASE:BCHEUR",  "COINBASE:LTCEUR" ]
+
+const timeFrames =  { one: ['3', '15', '30',  '180'], two: ['1', '30', '60',  '180'], three: ['30','60', '120', 'D']}
 
 function App() {
 
-const [coiny, setCoiny] = useState("COINBASE:BTCEUR");
-const [timeFrame, setTimeFrame] = useState(['3', '15', '45' , '180'])
-const [indicators, setIndicator] = useState(["BollingerBandsR@tv-basicstudies"]);
+const [coin, setCoin] = useState(sym[1]);
+const [timeFrame, setTimeFrame] = useState(timeFrames.one)
+const [indicators, setIndicator] = useState(indicator.one);
 
 const [mode, setMode] = useState(true);
 
-const makeDark = () => {
-  setMode( mode  ? false : true);
-}
+const dark = mode  ? 'light' : 'dark';
 
-return (
+const navButtons =  [
 
-<div className="App">
+                     { text: mode ? 'Dark Mode' : 'Light Mode', mode: dark, funct: function(){setMode(mode ? false : true)} },
 
-<div className="Navbar">
+                     { text: 'ETH', symbol: sym[0], funct: function(){setCoin(this.symbol)} },
+                     { text: 'BTC', symbol: sym[1], funct: function(){setCoin(this.symbol)} },
+                     { text: 'BCH', symbol: sym[2], funct: function(){setCoin(this.symbol)} },
+                     { text: 'LTC', symbol: sym[3], funct: function(){setCoin(this.symbol)} },
 
-<button onClick={() => setCoiny("COINBASE:ETHEUR")}>ETH</button>
-<button onClick={() => setCoiny("COINBASE:BTCEUR")}>BTC</button>
-<button onClick={() => setCoiny("COINBASE:BCHEUR")}>BCH</button>
-<button onClick={() => setCoiny("COINBASE:LTCEUR")}>LTC</button>
+                    {text: '1 Indicators', indicators: indicator.one, funct: function(){setIndicator(this.indicators)}  },
+                    {text: '2 Indicators', indicators: indicator.two, funct: function(){setIndicator(this.indicators)}  },
+                    {text: 'Short-Term'  , timeFrame: timeFrames.two, funct: function(){setTimeFrame(this.timeFrame)}   },
+                    {text: 'Medium-Term' , timeFrame: timeFrames.three, funct: function(){setTimeFrame(this.timeFrame)} },
+                    ]
+                    
+const charts = [{ id:0, class:'chart-window', symbol:coin}, { id:1, class:'chart-window', symbol:coin}, 
+                    { id:2, class:'chart-window', symbol:coin}, { id:3, class:'scalp-chart' , symbol:coin}]
+   
+ return (
 
-<button onClick={() => setTimeFrame( [ '3', '15', '45' , '180'])}> Short-term</button>
-<button onClick={() => setTimeFrame([ '45', '60', '180' , 'D'])}> Medium-term</button>
+<div className={"App " + dark}>
+<nav className={"navbar " + dark}>
 
-<button onClick={() => setIndicator([ "MAVolumeWeighted@tv-basicstudies"])}>VWMA</button>
+{navButtons.map( button => <span key={button.text} className={dark} onClick={() => button.funct()} > {button.text}</span> )}
 
-<button onClick={makeDark}>{ mode ? 'Dark' : 'Light' }</button>
+{links.map(link => <a key={link.name} className={dark} target="_tab" href={link.name}> {link.text}</a> )}
 
-{/* add links */}
-<button><a target="_tab"  href="https://coinfarm.online/index.asp?tcs=1h">CoinFarm</a></button>
-<button><a target="_tab" href="https://coinmarketcap.com/">CMC</a></button>
-<button><a target="_tab" href="https://www.bitfinex.com/trading">Bitfinex</a></button>
-<button><a target="_tab" href="http://data.bitcoinity.org/markets/bidask_sum/7d/USD/coinbase?bp=5&bu=c&t=m"> Bids vs Sums</a></button>
+</nav>
 
-
-</div >
-
-<div className="chart-window">
-
-    <TradingViewWidget
-    symbol={coiny}
-    theme= {mode ?  'Light': 'Dark' }
-    BarStyles='HEIKIN_ASHI'
-    locale="en"
-    Style='8'
-    interval={timeFrame[0]}
-    studies={indicators}  
-    hide_side_toolbar={false}
-    autosize      
-    /> 
-
+{charts.map( chart => 
+    
+<div key={chart.id} className={chart.class}>
+<TradingViewWidget
+symbol={chart.symbol}
+theme= {mode ?  'Light': 'Dark' }
+BarStyles='HEIKIN_ASHI'
+locale="en"
+Style='8'
+interval={timeFrame[chart.id]}
+studies={indicators}  
+hide_side_toolbar={false}
+autosize = {true}        
+/>  
 </div>
-
-<div className="chart-window">
-
-    <TradingViewWidget
-    symbol={coiny}
-    theme= {mode ?  'Light': 'Dark' }
-    BarStyles='HEIKIN_ASHI'
-    locale="en"
-    Style='8'
-    interval={timeFrame[1]}
-    studies={indicators}  
-    hide_side_toolbar={false}
-    autosize      
-    /> 
-
-</div>
-
-<div className="chart-window">
-
-    <TradingViewWidget
-    symbol={coiny}
-    theme= {mode ?  'Light': 'Dark' }
-    BarStyles='HEIKIN_ASHI'
-    locale="en"
-    Style='8'
-    interval={timeFrame[2]}
-    studies={indicators}  
-    hide_side_toolbar={false}
-    autosize      
-    /> 
-  
- </div>
-
-<div className="scalp-chart">
-
-    <TradingViewWidget
-    symbol={coiny}
-    theme= {mode ?  'Light': 'Dark' }
-    BarStyles='HEIKIN_ASHI'
-    locale="en"
-    Style='8'
-    interval={timeFrame[3]}
-    studies={indicators}  
-    hide_side_toolbar={false}
-    autosize      
-    /> 
-
-</div>
+)}
 
 </div>
 
